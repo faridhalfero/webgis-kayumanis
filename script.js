c702f84d3d977f3e1eb5d63db6e117ab9397a49d
faridhalfero <144414011+faridhalfero@users.noreply.github.com>
2025-07-16 15:45:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
 // Inisialisasi peta
-const map = L.map('map').setView([-0.5, 100.5], 9); // Ubah sesuai wilayah kayu manis
+const map = L.map('map').setView([-0.5, 100.5], 9);
 
-// Basemap citra satelit Esri
-const satellite = L.tileLayer(
-  'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-  {
+// --- Basemap: Peta Nama Jalan (OpenStreetMap) ---
+const basemapOSM = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '© OpenStreetMap contributors',
+  maxZoom: 19
+}).addTo(map); // Aktif default
+
+// --- Basemap: Citra Satelit Esri ---
+const basemapEsri = L.tileLayer(
+  'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
     attribution: 'Tiles © Esri & Contributors',
     maxZoom: 19
   }
-).addTo(map);
+);
 
-// Tampilkan GeoJSON
+// --- Basemap: Esri Streets (3D style) ---
+const basemapEsriStreets = L.tileLayer(
+  'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles © Esri & Contributors',
+    maxZoom: 19
+  }
+);
+
+// --- Layer Control ---
+const baseMaps = {
+  "Peta Jalan (OpenStreetMap)": basemapOSM,
+  "Citra Satelit (Esri)": basemapEsri,
+  "Peta Jalan 3D (Esri Streets)": basemapEsriStreets
+};
+L.control.layers(baseMaps).addTo(map);
+
+// --- Tampilkan Layer GeoJSON ---
 fetch('Kayumanis.geojson')
   .then(res => res.json())
   .then(data => {
     const geoLayer = L.geoJSON(data, {
       style: {
-        color: "#ff7800",
+        color: "#ff6600",
         weight: 2,
-        fillOpacity: 0.5
+        fillOpacity: 0.4
       },
       onEachFeature: function (feature, layer) {
         let info = "";
@@ -29,7 +50,6 @@ fetch('Kayumanis.geojson')
       }
     }).addTo(map);
 
-    // Zoom ke area poligon
     map.fitBounds(geoLayer.getBounds());
   })
   .catch(err => {
