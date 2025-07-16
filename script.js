7a447e183fa51d000986c6b0adef345140e9f527
faridhalfero <144414011+faridhalfero@users.noreply.github.com>
2025-07-16 15:17:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-const map = L.map('map').setView([-0.5, 100.5], 9); // Sesuaikan koordinat
+const map = L.map('map').setView([-0.5, 100.5], 9);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-// Load GeoJSON
+const satellite = L.tileLayer(
+  'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles © Esri'
+});
+
+L.control.layers({
+  "Peta Biasa": osm,
+  "Citra Satelit": satellite
+}).addTo(map);
+
 fetch('Kayumanis.geojson')
   .then(res => res.json())
   .then(data => {
-    L.geoJSON(data).addTo(map);
+    const geoLayer = L.geoJSON(data).addTo(map);
+    map.fitBounds(geoLayer.getBounds());
   });
